c44e1d882c5842590cc43a9005d11dc0d9ab47b2
theroyalimage <delightadediran21@gmail.com>
2024-12-04 08:54:49 +0100

    urls

--- crms/urls.py
+++ crms/urls.py
@@ -24,7 +24,7 @@ from rest_framework import permissions
 
 schema_view = get_schema_view(
     openapi.Info(
-        title="My API",
+        title="Course Resources Management System API",
         default_version='v1',
         description="Test description",
         terms_of_service="https://www.google.com/policies/terms/",
@@ -37,7 +37,7 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    re_path(r'^api/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('', include('courses.urls')),
 ]
 if settings.DEBUG:
