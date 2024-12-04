77b5c9188fecad5333e731ba330ad79a34738481
theroyalimage <delightadediran21@gmail.com>
2024-12-04 09:10:53 +0100

    collectstatic

--- crms/urls.py
+++ crms/urls.py
@@ -41,4 +41,4 @@ urlpatterns = [
     path('', include('courses.urls')),
 ]
 if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_ROOT, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
